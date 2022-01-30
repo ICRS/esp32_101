c6d9cab75456bdf96a2598330307f0703c69f13d
ohmdelta <64962148+ohmdelta@users.noreply.github.com>
2022-01-30 14:49:16 +0000

    created setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(    
+    name='drivelib',
+    packages=find_packages(include=['drive']),
+    version='0.1.0',
+    description='esp32 micropython drivebase library',
+    author='Haotian',
+    )
